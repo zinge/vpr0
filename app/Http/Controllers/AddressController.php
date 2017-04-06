fd5be4cbcb9058c0bb9f530a9f2fb99c234cdb96
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2017-04-06 19:27:01 +0300

    modified:   app/Http/Controllers/AddressController.php 	modified:   routes/web.php

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -7,79 +7,95 @@ use Illuminate\Http\Request;
 
 class AddressController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
+  /**
+  * Display a listing of the resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index()
+  {
+    //
+    $pageSruture = [
+      ['type' => 'text', 'desc' => 'city'],
+      ['type' => 'text', 'desc' => 'street'],
+      ['type' => 'text', 'desc' => 'house'],
+      ['type' => 'checkbox', 'desc' => 'active'],
+    ];
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Address  $address
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Address $address)
-    {
-        //
-    }
+    return response()
+    ->with('pageSruture', $pageSruture);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Address  $address
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Address $address)
-    {
-        //
-    }
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Address  $address
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Address $address)
-    {
-        //
-    }
+  /**
+  * Show the form for creating a new resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Address  $address
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Address $address)
-    {
-        //
-    }
+  /**
+  * Store a newly created resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @return \Illuminate\Http\Response
+  */
+  public function store(Request $request)
+  {
+    //
+  }
+
+  /**
+  * Display the specified resource.
+  *
+  * @param  \App\Address  $address
+  * @return \Illuminate\Http\Response
+  */
+  public function show(Address $address)
+  {
+    //
+  }
+
+  /**
+  * Show the form for editing the specified resource.
+  *
+  * @param  \App\Address  $address
+  * @return \Illuminate\Http\Response
+  */
+  public function edit(Address $address)
+  {
+    //
+  }
+
+  /**
+  * Update the specified resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  \App\Address  $address
+  * @return \Illuminate\Http\Response
+  */
+  public function update(Request $request, Address $address)
+  {
+    //
+  }
+
+  /**
+  * Remove the specified resource from storage.
+  *
+  * @param  \App\Address  $address
+  * @return \Illuminate\Http\Response
+  */
+  public function destroy(Address $address)
+  {
+    //
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,18 @@ Auth::routes();
 Route::get('/home', 'HomeController@index');
 
 Route::resource('/spravochnik', 'SpravochnikController');
+
+Route::resource('/address', 'AddressController');
+Route::resource('/department', 'DepartmentController');
+Route::resource('/employee', 'EmployeeController');
+Route::resource('/equip', 'EquipController');
+Route::resource('/equip-model', 'EquipModelController');
+Route::resource('/equip-type', 'EquipTypeController');
+Route::resource('/ip-phone', 'IpPhoneController');
+Route::resource('/manufacturer', 'MannufacturerController');
+Route::resource('/mobile-limit', 'MobileLimitController');
+Route::resource('/mobile-phone', 'MobilePhoneController');
+Route::resource('/mobile-type', 'MobileTypeController');
+Route::resource('/phone', 'PhoneController');
+Route::resource('/phone-type', 'PhoneTypeController');
+Route::resource('/workplace', 'WorkplaceController');
