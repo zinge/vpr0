a414396a3687ed7d2376c7ccf8da8dc7316a45a5
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2017-05-24 20:49:07 +0300

    modified:   resources/views/employee/index.blade.php 	modified:   resources/views/modal/_list.blade.php

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -10,18 +10,18 @@
           <div class="collapse" id="{{$pageHref}}AddForm">
             <div class="well">
               <div class="panel">
-                <div class="panel panel-heading">заполните таблицу</div>
+                {{--<div class="panel panel-heading">заполните форму</div>--}}
                 <div class="panel-body">
 
                   <form method="post" action="{{url($pageHref)}}" class="form-horizontal" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
-                  {{ csrf_field() }}
+                    {{ csrf_field() }}
 
-                  @foreach ($pageStructure as $pageElement)
-                    {{--
+                    @foreach ($pageStructure as $pageElement)
+                      {{--
                       ['type' => 'text', 'field' => 'city', 'desc' => 'город'],
-                    --}}
-                    @include("modal._".$pageElement['type'], $pageElement)
-                  @endforeach
+                      --}}
+                      @include("modal._".$pageElement['type'], $pageElement)
+                    @endforeach
 
                     <div class="form-group">
                       <div class="col-sm-offset-4 col-sm-8">
@@ -70,24 +70,24 @@
   <script src="js/spravochnik1.js" charset="utf-8"></script>
   <script>
 
-        new Vue({
-          el: '#{{$pageHref}}AddForm',
+  new Vue({
+    el: '#{{$pageHref}}AddForm',
 
-          data: {
-            form: new Form({
-              @foreach ($pageStructure as $value)
-                {{$value['field']}}: ''{{$loop->last ? '' : ','}}
-              @endforeach
-            })
-          },
+    data: {
+      form: new Form({
+        @foreach ($pageStructure as $value)
+        {{$value['field']}}: ''{{$loop->last ? '' : ','}}
+        @endforeach
+      })
+    },
 
-          methods: {
-            onSubmit() {
-                this.form.post('{{url($pageHref)}}')
-                  .then(response => location.reload());
-            }
-          }
-        });
+    methods: {
+      onSubmit() {
+        this.form.post('{{url($pageHref)}}')
+        .then(response => location.reload());
+      }
+    }
+  });
 
   </script>
 
