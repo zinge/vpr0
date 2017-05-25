1c8217ec9d38205c3987804654bb03c4ee6c8516
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2017-05-25 13:00:13 +0300

    modified:   resources/views/modal/_list.blade.php

--- resources/views/modal/_list.blade.php
+++ resources/views/modal/_list.blade.php
@@ -11,29 +11,33 @@
     <div class="alert alert-danger" v-if="form.errors.has('{{$field}}')" v-text="form.errors.get('{{$field}}')"></div>
   </div>
   <div class="col-sm-1">
+
     <button type="button" class="btn btn-primary" name="button" data-toggle="modal" data-target="#{{$field}}Modal">+</button>
-  </div>
-</div>
 
-{{--start modal--}}
-<div class="modal fade" id="{{$field}}Modal" tabindex="-1" role="dialog" aria-labelledby="{{$field}}ModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      {{-- @include('modal.index', $pageElement)--}}
-      <div class="modal-header">
-        {{----}}
-      </div>
-      <div class="modal-body">
-        <form class="form-horizontal" action="#" method="post">
-          {{ csrf_field() }}
+  {{--start modal--}}
+  <div class="modal fade" id="{{$field}}Modal" tabindex="-1" role="dialog" aria-labelledby="{{$field}}ModalLabel">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        {{-- @include('modal.index', $pageElement)--}}
+        <div class="modal-header">
+          {{----}}
+        </div>
+        <div class="modal-body">
+{{--
+          <form class="form-horizontal" action="#" method="post">
+            {{ csrf_field() }}
 
 
-        </form>
-      </div>
-      <div class="modal-footer">
+          </form>
+--}}
+        </div>
+        <div class="modal-footer">
 
+        </div>
       </div>
     </div>
   </div>
+  {{--end modal--}}
+
+  </div>
 </div>
-{{--end modal--}}
