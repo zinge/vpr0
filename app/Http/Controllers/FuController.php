61f8357fc8e848e81f24fc4561f3a98cf3c547ac
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2017-08-11 09:13:24 +0300

    modified:   app/Http/Controllers/FuController.php

--- app/Http/Controllers/FuController.php
+++ app/Http/Controllers/FuController.php
@@ -9,140 +9,145 @@ use Storage;
 
 class FuController extends Controller
 {
-  public function __construct()
-  {
-    $this->middleware('auth');
-  }
-
-  private function createPageStructure()
-  {
-    $pageStructure = [
-      ['type' => 'file', 'field' => 'file_name', 'desc' => 'имя файла'],
-      ['type' => 'none', 'field' => 'mime_type', 'desc' => 'тип файла'],
-      ['type' => 'none', 'field' => 'original_filename', 'desc' => 'оригинальное имя файла'],
-    ];
-
-    return $pageStructure;
-  }
-
-  private function createPageParams($id)
-  {
-    $pageParams = [];
-
-    $files = $id ? Fu::find([$id]) : Fu::get();
-
-    foreach ($files as $file) {
-      array_push($pageParams, [
-        'id' => $file->id,
-        'file_name' => $file->file_name,
-        'mime_type' => $file->mime_type,
-        'original_filename' => $file->original_filename,
-      ]);
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
 
-    return $pageParams;
-  }
-  /**
-  * Display a listing of the resource.
-  *
-  * @return \Illuminate\Http\Response
-  */
-  public function index()
-  {
-    //
-    return view('fileupload.index')
-    ->with('pageStructure', $this->createPageStructure())
-    ->with('pageParams', $this->createPageParams(''))
-    ->with('pageTitle', 'файл')
-    ->with('pageHref', 'fu');
-  }
+    private function createPageStructure()
+    {
+        $pageStructure = [
+        ['type' => 'file', 'field' => 'file_name', 'desc' => 'имя файла'],
+        ['type' => 'none', 'field' => 'mime_type', 'desc' => 'тип файла'],
+        ['type' => 'none', 'field' => 'original_filename', 'desc' => 'оригинальное имя файла'],
+        ];
 
-  /**
-  * Show the form for creating a new resource.
-  *
-  * @return \Illuminate\Http\Response
-  */
-  public function create()
-  {
-    //
-  }
+        return $pageStructure;
+    }
 
-  /**
-  * Store a newly created resource in storage.
-  *
-  * @param  \Illuminate\Http\Request  $request
-  * @return \Illuminate\Http\Response
-  */
-  public function store(Request $request)
-  {
-    //
-    $this->validate($request,[
-      'file_name' => ['file', 'mimes:csv,txt', 'mimetypes:text/csv,text/plain']
-    ]);
+    /**
+     * Create main table in page params
+     *
+     * @return $pageParams[]
+     */
+    private function createPageParams($id)
+    {
+        $pageParams = [];
 
-    $file = $request->file('file_name');
-    $fileExt = $file->getClientOriginalExtension();
-    Storage::disk('local')->put(
-      'files/'.$file->getFilename().'.'.$fileExt,
-      file_get_contents($file->getRealPath())
-    );
-    Fu::create([
-      'mime_type' => $file->getClientMimeType(),
-      'original_filename' => $file->getClientOriginalName(),
-      'file_name' => 'files/'.$file->getFilename().'.'.$fileExt,
-    ]);
-    return redirect('/fu');
-  }
+        $files = $id ? Fu::find([$id]) : Fu::get();
 
-  /**
-  * Display the specified resource.
-  *
-  * @param  \App\Fu  $fu
-  * @return \Illuminate\Http\Response
-  */
-  public function show(Fu $fu)
-  {
-    //
-    dd($fu);
-  }
+        foreach ($files as $file) {
+            array_push($pageParams, [
+            'id' => $file->id,
+            'file_name' => $file->file_name,
+            'mime_type' => $file->mime_type,
+            'original_filename' => $file->original_filename,
+            ]);
+        }
 
-  /**
-  * Show the form for editing the specified resource.
-  *
-  * @param  \App\Fu  $fu
-  * @return \Illuminate\Http\Response
-  */
-  public function edit(Fu $fu)
-  {
-    //
-  }
+        return $pageParams;
+    }
 
-  /**
-  * Update the specified resource in storage.
-  *
-  * @param  \Illuminate\Http\Request  $request
-  * @param  \App\Fu  $fu
-  * @return \Illuminate\Http\Response
-  */
-  public function update(Request $request, Fu $fu)
-  {
-    //
-  }
+    /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+        //
+        return view('fileupload.index')
+        ->with('pageStructure', $this->createPageStructure())
+        ->with('pageParams', $this->createPageParams(''))
+        ->with('pageTitle', 'файл')
+        ->with('pageHref', 'fu');
+    }
 
-  /**
-  * Remove the specified resource from storage.
-  *
-  * @param  \App\Fu  $fu
-  * @return \Illuminate\Http\Response
-  */
-  public function destroy(Fu $fu)
-  {
-    //
-    Storage::delete($fu->file_name);
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function create()
+    {
+        //
+    }
 
-    $fu->delete();
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request, [
+        'file_name' => ['file', 'mimes:csv,txt', 'mimetypes:text/csv,text/plain']
+        ]);
 
-    return redirect('/fu');
+        $file = $request->file('file_name');
+        $fileExt = $file->getClientOriginalExtension();
+        Storage::disk('local')->put(
+            'files/'.$file->getFilename().'.'.$fileExt,
+            file_get_contents($file->getRealPath())
+        );
+        Fu::create([
+        'mime_type' => $file->getClientMimeType(),
+        'original_filename' => $file->getClientOriginalName(),
+        'file_name' => 'files/'.$file->getFilename().'.'.$fileExt,
+        ]);
+        return redirect('/fu');
+    }
 
-  }
+    /**
+    * Display the specified resource.
+    *
+    * @param  \App\Fu  $fu
+    * @return \Illuminate\Http\Response
+    */
+    public function show(Fu $fu)
+    {
+        //
+        dd($fu);
+    }
+
+    /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  \App\Fu  $fu
+    * @return \Illuminate\Http\Response
+    */
+    public function edit(Fu $fu)
+    {
+        //
+    }
+
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \App\Fu  $fu
+    * @return \Illuminate\Http\Response
+    */
+    public function update(Request $request, Fu $fu)
+    {
+        //
+    }
+
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  \App\Fu  $fu
+    * @return \Illuminate\Http\Response
+    */
+    public function destroy(Fu $fu)
+    {
+        //
+        Storage::delete($fu->file_name);
+
+        $fu->delete();
+
+        return redirect('/fu');
+    }
 }
